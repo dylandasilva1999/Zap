c0805568c90b6ccb193656063a2e0b0113586c76
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2021-03-11 10:58:24 +0200

    Add Selected Answer Functionality
    -Added selected and default on answer clicked states.

--- app/src/main/java/com/example/zap/GeneralQuizActivity.kt
+++ app/src/main/java/com/example/zap/GeneralQuizActivity.kt
@@ -1,5 +1,7 @@
 package com.example.zap
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,8 +9,15 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 
-class GeneralQuizActivity : AppCompatActivity() {
+class GeneralQuizActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mGeneralQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_general_quiz)
@@ -17,10 +26,27 @@ class GeneralQuizActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         //Get the General Questions List
-        val generalQuestionsList = Constants.generalQuestions()
+        mGeneralQuestionsList = Constants.generalQuestions()
 
-        val currentPosition = 1
-        val generalQuestion: Question? = generalQuestionsList[currentPosition - 1]
+        setGeneralQuestion()
+
+        //Get General Question Options
+        var tv_general_option_one = findViewById<TextView>(R.id.tv_general_option_one)
+        var tv_general_option_two = findViewById<TextView>(R.id.tv_general_option_two)
+        var tv_general_option_three = findViewById<TextView>(R.id.tv_general_option_three)
+
+        tv_general_option_one.setOnClickListener(this)
+        tv_general_option_two.setOnClickListener(this)
+        tv_general_option_three.setOnClickListener(this)
+
+    }
+
+    private fun setGeneralQuestion() {
+
+        mCurrentPosition = 1
+        val generalQuestion = mGeneralQuestionsList!![mCurrentPosition-1]
+
+        defaultOptionsView()
 
         //Get General Question Items
         var general_progressbar = findViewById<ProgressBar>(R.id.general_progressbar)
@@ -33,8 +59,8 @@ class GeneralQuizActivity : AppCompatActivity() {
         var tv_general_option_two = findViewById<TextView>(R.id.tv_general_option_two)
         var tv_general_option_three = findViewById<TextView>(R.id.tv_general_option_three)
 
-        general_progressbar.progress = currentPosition
-        tv_general_progress.text = "$currentPosition" + "/" + general_progressbar.max
+        general_progressbar.progress = mCurrentPosition
+        tv_general_progress.text = "$mCurrentPosition" + "/" + general_progressbar.max
         tv_general_question.text = generalQuestion!!.question
         general_category_bottom_image.setImageResource(generalQuestion.categoryImage)
 
@@ -42,5 +68,58 @@ class GeneralQuizActivity : AppCompatActivity() {
         tv_general_option_one.text = generalQuestion.optionOne
         tv_general_option_two.text = generalQuestion.optionTwo
         tv_general_option_three.text = generalQuestion.optionThree
+
     }
+
+    private fun defaultOptionsView() {
+
+        var tv_general_option_one = findViewById<TextView>(R.id.tv_general_option_one)
+        var tv_general_option_two = findViewById<TextView>(R.id.tv_general_option_two)
+        var tv_general_option_three = findViewById<TextView>(R.id.tv_general_option_three)
+
+        val options = ArrayList<TextView>()
+        options.add(0, tv_general_option_one)
+        options.add(1, tv_general_option_two)
+        options.add(2, tv_general_option_three)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#131313"))
+            option.background = ContextCompat.getDrawable(
+                    this,
+                    R.drawable.option_style
+            )
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+
+        var tv_general_option_one = findViewById<TextView>(R.id.tv_general_option_one)
+        var tv_general_option_two = findViewById<TextView>(R.id.tv_general_option_two)
+        var tv_general_option_three = findViewById<TextView>(R.id.tv_general_option_three)
+
+        when(v?.id) {
+            R.id.tv_general_option_one ->{
+                selectedOptionsView(tv_general_option_one, 1)
+            }
+            R.id.tv_general_option_two ->{
+                selectedOptionsView(tv_general_option_two, 2)
+            }
+            R.id.tv_general_option_three ->{
+                selectedOptionsView(tv_general_option_three, 3)
+            }
+        }
+    }
+
+    private fun selectedOptionsView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#FFFFFF"))
+        tv.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.general_selected_option_style
+        )
+    }
+
 }
