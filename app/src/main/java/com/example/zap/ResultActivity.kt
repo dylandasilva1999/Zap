482d3fd7386d5a32c2fbd57b099d2bcdacded1b5
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2021-03-25 11:10:42 +0200

    Attempt Sound Effect

--- app/src/main/java/com/example/zap/ResultActivity.kt
+++ app/src/main/java/com/example/zap/ResultActivity.kt
@@ -2,16 +2,31 @@ package com.example.zap
 
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.media.SoundPool
+import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
+
+    private var soundPool: SoundPool? = null
+    private val soundId = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        soundPool = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+        soundPool!!.load(baseContext, R.raw.results_sound_effect, 1)
+
+        fun playSound(resultActivity: ResultActivity) {
+            soundPool?.play(soundId, 1F, 1F, 0, 0, 1F)
+        }
 
         //Make the View FullScreen
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
